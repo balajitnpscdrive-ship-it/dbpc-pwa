6d4bee4fe5db8037e6ce68c716f19249486cf56d
balajitnpscdrive-ship-it <balajitnpscdrive@gmail.com>
2026-03-11 19:57:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,6 @@
 self.addEventListener('install', (e) => {
-  console.log('PWA Service Worker Installed');
+  self.skipWaiting();
 });
-
 self.addEventListener('fetch', (e) => {
-  // Required to be a PWA, even if it just passes through
   e.respondWith(fetch(e.request));
 });
