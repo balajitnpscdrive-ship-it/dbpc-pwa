00aaf603e2127e333f86f3392450557fa4810cf7
balajitnpscdrive-ship-it <balajitnpscdrive@gmail.com>
2026-03-11 20:17:56 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
+const CACHE_NAME = 'gas-pwa-v1';
+const ASSETS = ['./index.html', './manifest.json'];
+
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
